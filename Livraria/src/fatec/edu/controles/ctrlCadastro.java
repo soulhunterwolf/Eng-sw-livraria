aa43fd17d4ed0bd456cc8384eddb1f062186766e
Kathleen C Pereira <kathcp94@gmail.com>
2016-04-12 00:00:35 -0300

    Criação de packages e duas classes para que elas não fiquem "peladas"

---
+++ Livraria/src/fatec/edu/controles/ctrlCadastro.java
@@ -0,0 +1,8 @@
+package fatec.edu.controles;
+
+public class ctrlCadastro {
+
+	public void procurarCliente(){}
+	
+	public void procurarUsuario(){}
+}
---
+++ Livraria/src/fatec/edu/entidades/Cliente.java
@@ -0,0 +1,28 @@
+package fatec.edu.entidades;
+
+public class Cliente {
+
+	private String name;
+	private String endereco;
+	private int telefone;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	
+}
