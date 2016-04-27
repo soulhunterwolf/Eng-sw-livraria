393f1b1fb3782f1da12783ceeee8a87eb5a0e02f
Giovanni Gonçalves Braga de Sousa <giovanni.ggbs@gmail.com>
2016-04-27 15:08:26 -0300

    Scrumel - Criação do pacote de implementação

---
+++ Livraria/src/fatec/edu/impl/LivroDaoImpl.java
@@ -0,0 +1,58 @@
+package fatec.edu.impl;
+
+import java.util.List;
+
+import fatec.edu.dao.ILivroDao;
+import fatec.edu.entidades.Livro;
+
+public class LivroDaoImpl implements ILivroDao {
+
+	@Override
+	public void adicionar(Livro l) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void remover(int isbn) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void atualizar(int isbn, Livro l) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Livro> pesquisaTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Livro> pesquisaPorTitulo(String titulo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Livro> pesquisaPorAutor(String autor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Livro> pesquisaPorEditora(String editora) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Livro> pesquisaPorCategoria(String categoria) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
