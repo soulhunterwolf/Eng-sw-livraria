f29ce3f80eb83286ab836e5da2614a8192f649c9
Giovanni Gonçalves Braga de Sousa <giovanni.ggbs@gmail.com>
2016-04-27 15:04:47 -0300

    Scrumel - Criação de pacote Bean

---
+++ Livraria/src/fatec/edu/bean/LivroMB.java
@@ -0,0 +1,12 @@
+package fatec.edu.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroMB implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
