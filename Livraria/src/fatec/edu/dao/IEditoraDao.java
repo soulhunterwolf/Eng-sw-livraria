457f6831968043706ca9b9f6ad84cc0ea749ce74
Giovanni Gonçalves Braga de Sousa <giovanni.ggbs@gmail.com>
2016-05-03 09:04:54 -0300

    Scrumel - alteração de estrutura

--- Livraria/src/fatec/edu/dao/IEditoraDao.java
+++ Livraria/src/fatec/edu/dao/IEditoraDao.java
@@ -3,15 +3,15 @@ package fatec.edu.dao;
 
 import java.util.List;
 
-import fatec.edu.entidades.Cliente;
+import fatec.edu.entidades.Editora;
 
 public interface IEditoraDao {
 	
-	public void adicionar(Cliente a);
+	public void adicionar(Editora a);
 	public void remover(String nome) ;
-	public void atualizar(String nome, Cliente a);
-	// métodos de pesquisas do Cliente
-	public List<Cliente> pesquisaTodos();
-	public List<Cliente> pesquisaPorNome(String nome);
+	public void atualizar(String nome, Editora a);
+	// mï¿½todos de pesquisas do Editora
+	public List<Editora> pesquisaTodos();
+	public List<Editora> pesquisaPorNome(String nome);
 }
 
