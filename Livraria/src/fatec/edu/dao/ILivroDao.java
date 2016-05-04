d53286e38c9a49957d79d924321b64e0815a13ff
Giovanni Gonçalves Braga de Sousa <giovanni.ggbs@gmail.com>
2016-05-04 16:04:30 -0300

    Scrumel - alteração 2

--- Livraria/src/fatec/edu/dao/ILivroDao.java
+++ Livraria/src/fatec/edu/dao/ILivroDao.java
@@ -10,9 +10,10 @@ public interface ILivroDao {
 	public void adicionar(Livro l);
 	public void remover(int isbn) ;
 	public void atualizar(int isbn, Livro l);
-	// métodos de pesquisas por título, autor, editora ou categoria do livro
+	// mï¿½todos de pesquisas por tï¿½tulo, autor, editora ou categoria do livro
 	public List<Livro> pesquisaTodos();
 	public List<Livro> pesquisaPorTitulo(String titulo);
+	public List<Livro> pesquisaPorIsbn(int isbn);
 	public List<Livro> pesquisaPorAutor(String autor);
 	public List<Livro> pesquisaPorEditora(String editora);
 	public List<Livro> pesquisaPorCategoria(String categoria);
